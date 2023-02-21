0600d97aee368e019582ef5780d14387cb3085cd
Max Swain <89113255+maxdswain@users.noreply.github.com>
2023-02-21 21:43:26 +0000

    Parallelise data collection

--- collect_data.sh
+++ collect_data.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-seeds=(11 3459 1346653 246 38479)
+SEEDS=(11 3459 1346653 246 38479)
 make
-sed -i "/seed/s/= .*/= ${seeds[0]}/" input.toml
-./main
 for i in {1..4}
 do
-    mv energy_data.out energy_data_$i.out
-    mv position_data.out position_data_$i.out
-    sed -i "/seed/s/= .*/= ${seeds[$i]}/" input.toml
-    ./main
+    sed -i "/seed/s/= .*/= ${SEEDS[$i]}/" input.toml
+    ./main & 
 done
+wait
 ./src/analysis.py
