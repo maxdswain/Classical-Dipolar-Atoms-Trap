dfe1de04244d3136313056318297d561c8cf20b1
Max Swain <89113255+maxdswain@users.noreply.github.com>
2023-03-30 14:33:13 +0100

    Added ability to do a 1D density plot for CASINO results

--- tools/plot_casino.py
+++ tools/plot_casino.py
@@ -34,5 +34,25 @@ def plot_density_contour() -> None:
     plt.savefig("density_contour.png")
 
 
+def plot_densities() -> None:
+    plt.rcParams.update({"font.size": 20})
+    _, ax = plt.subplots(figsize=(12, 9))
+    dipole_moments, qmc = [0.0707, 2, 5, 100], ["VMC", "DMC"]
+    for i, density in enumerate(
+        map(
+            lambda f: np.delete(np.loadtxt(f), 2, 1).reshape(BINS_X, BINS_Y, 3),
+            sorted([f for f in os.listdir(".") if os.path.isfile(f) and "2Dplot" in f]),
+        )
+    ):
+        ax.plot(
+            density[0, :, 0],
+            np.sum(density[:, :, 2], axis=0) / BINS_X,
+            label=f"{qmc[0 if i < 4 else 1]} $|p|={dipole_moments[i % 4]}$",
+        )
+    ax.set(xlabel="$x$ position", ylabel="Number density (a.u.)")
+    ax.legend(loc="upper right")
+    plt.savefig("densities.png")
+
+
 if __name__ == "__main__":
     plot_density_contour()
